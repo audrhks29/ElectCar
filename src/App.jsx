5630cc20190502bc0ab1bc5049b5d4284f093fad
audrhks29 <audrhks29@gmail.com>
2023-07-02 01:48:52 +0900

    ElectCar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 
 import GlobalStyle from './styled/GlobalStyle';
-
 import CommonHeader from './components/CommonHeader';
 import CommonFooter from './components/CommonFooter';
-
-import { Routes, Route, BrowserRouter, useLocation } from 'react-router-dom';
 
 import Home from './pages/Home';
 import TamlaIsCar from './pages/TamlaIsCar';
@@ -23,21 +21,19 @@ const ScrollToTop = () => {
 
   return null;
 };
+
 const App = () => {
   return (
     <div className='wrap' style={{ position: "relative" }}>
-
       <GlobalStyle />
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <ScrollToTop />
         <CommonHeader />
         <Routes>
-          <Route path="/" index element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/info" element={<TamlaIsCar />} />
-          <Route path="noticeList">
-            <Route index element={<NoticeList />} />
-            <Route path=":noticeID" element={<NoticeDetail />} />
-          </Route>
+          <Route path="/noticeList" element={<NoticeList />} />
+          <Route path="/noticeList/:noticeID" element={<NoticeDetail />} />
           <Route path="/terms" element={<Terms />} />
           <Route path="/privacy" element={<Privacy />} />
         </Routes>
